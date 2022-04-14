d4e61bebfba5b77d5b04b16f97af2a8a95bca25f
alexsomerville <asomerville1@my.bcit.ca>
2022-04-14 11:25:19 -0700

    added svg's

---
+++ fashoid/pages/store.js
@@ -0,0 +1,14 @@
+import Background from "../comps/background";
+import Img from "../comps/image";
+
+export default function Store() {
+    return <div>
+    <Background img="/Home_bg.svg" />
+    <Img img="/storelady.svg" />
+    <Img img="/lightbar.svg" />
+    <Img img="/jeanrack.svg" />
+    <Img img="/topsrack.svg" />
+
+
+    </div>
+}

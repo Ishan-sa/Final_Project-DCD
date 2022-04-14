858d3c97d55fd3742980feba22d35d5789699930
alexsomerville <asomerville1@my.bcit.ca>
2022-04-14 16:53:07 -0700

    store page fixed padding

--- fashoid/comps/image.js
+++ fashoid/comps/image.js
@@ -4,15 +4,18 @@ import Image from "next/image";
 const AllImages = styled.div`
     display: flex;
     justify-content: center;
-    padding: 20px 0px 20px 0px;
+    padding: ${props=>props.p};
+    position: ${props=>props.po};
 `
 
 export default function Img({
     img = "/vercel.svg",
     w = 65,
-    h = 65
+    h = 65,
+    padding = "20px 0px 20px 0px",
+    position = "relative"
 }) {
-    return <AllImages>
+    return <AllImages p={padding} po={position}>
         <Image src={img} width={w} height={h} />
     </AllImages>
 }
--- fashoid/pages/store.js
+++ fashoid/pages/store.js
@@ -3,17 +3,18 @@ import Img from "../comps/image";
 import styled from "styled-components";
 
 
-
 export default function Store() {
     return <div>
     <Background img="/Home_bg.svg" />
     <Img img="/storelady.svg" w="300" h="300"/>
-    <Img img="/lightlines.svg" w="390" h="9"/>
-    <Img img="/lightbar.svg" w="390" h="100" />
-    <Img img="/cabinet.svg" w="296" h="464" />
-    <Img img="/jeans3.svg"/>
-    <Img img="/topsrack.svg" w="342" h="189" />
-
-
+    <Img img="/lightlines.svg" w="390" h="9" padding="0px 0px 0px 0px"/>
+    <Img img="/lightbar.svg" w="390" h="100" padding="0px 0px 0px 0px"/>
+    <div w="296" h="464">
+        <Img img="/cabinet.svg" w="296" h="464" />
+        <Img img="/jeanshat.svg" w="221" h="37" po="relative"/>
+        <Img img="/foldedjeans.svg" w="221" h="37" po="relative"/>
+        <Img img="/jeans3.svg" w="213.67" h="179.41" po="relative"/>
+    </div>
+    <Img img="/topsrack.svg" w="342" h="189"/>
     </div>
 }

3074d77c9819a8d3ac6d2996253368ab1a24f064
Zahir Ali <zahirali@Zahirs-Air.vc.shawcable.net>
2022-04-16 22:35:08 -0700

    done with quiz, some styling left

--- fashoid/pages/Questions.js
+++ fashoid/pages/Questions.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import BodyText from "../comps/text-content";
 import Head1 from "../comps/heading";
-import Options from "../comps/Options";
 import Img from "../comps/image";
 
 
@@ -58,7 +57,257 @@ export default function Question1() {
     const r = useRouter();
     const { correct } = r.query;
     const { incorrect } = r.query;
+    const { q2 } = r.query;
+    const { correct2 } = r.query;
+    const { incorrect2 } = r.query;
+    const { q3 } = r.query;
+    const { correct3 } = r.query;
+    const { incorrect3 } = r.query;
 
+
+//Question 3 results
+if (correct3 === '') {
+    return <div>
+
+        <Background img="/Home_bg.svg" />
+        <BodyText txt="How many workers died in the Rana Plaza Collapse?" fontsize="20px" />
+        <OpsButton>843</OpsButton>
+        <OpsButton>512</OpsButton>
+        <CorrectButton>1132</CorrectButton>
+        <QuizStartBtn onClick={
+            () => r.replace({
+                pathname: "/Questions",
+                query: {
+                    q3: Number(q3) + 1
+                }
+            })
+        }>Next{q3}</QuizStartBtn>
+
+
+
+        <Img img="/Person + Bubble.svg"
+            w="200"
+            h="200" />
+
+    </div>
+
+}
+
+if (incorrect3 === '') {
+    return <div>
+
+        <Background img="/Home_bg.svg" />
+        <BodyText txt="Ah! You got it wrong :(" fontsize="20px" />
+        <BodyText txt="The correct answer is -" fontsize="10px" />
+        <CorrectButton>1132</CorrectButton>
+
+        <Img img="/Building falling.svg"
+
+            w="900"
+            h="1750" />
+
+        <QuizStartBtn onClick={
+            () => r.replace({
+                pathname: "/Questions",
+                query: {
+                    q3: Number(q3) + 1
+                }
+            })
+        }>Next{q3}</QuizStartBtn>
+
+
+    </div>
+
+}
+
+if (incorrect3 === '') {
+    return <div>
+
+
+        <Background img="/Home_bg.svg" />
+        <BodyText txt="Ah! You got it wrong :(" fontsize="20px" />
+        <BodyText txt="The correct answer is -" fontsize="10px" />
+        <CorrectButton>1132</CorrectButton>
+
+
+        <Img img="/Building falling.svg"
+
+            w="900"
+            h="1750" />
+        <QuizStartBtn onClick={
+            () => r.replace({
+                pathname: "/Questions",
+                query: {
+                    q3: Number(q3) + 1
+                }
+            })
+        }>Next{q3}</QuizStartBtn>
+
+    </div>
+
+}
+
+    //Question 3
+    if (q3 === '') {
+        return <div>
+
+            <Background img="/Home_bg.svg" />
+            <BodyText txt="How many workers died in the Rana Plaza Collapse? " fontsize="20px" />
+            <OpsButton onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        incorrect3: Number(incorrect3) + 1
+                    }
+                })
+            }>843</OpsButton>
+
+            <OpsButton onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        incorrect3: Number(incorrect3) + 1
+                    }
+                })
+
+            }>512{correct3}</OpsButton>
+
+            <OpsButton onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        correct3: Number(correct3) + 1
+                    }
+                })
+            }>1132</OpsButton>
+
+
+        </div>
+
+    }
+
+
+
+    //Question 2 results
+    if (correct2 === '') {
+        return <div>
+
+            <Background img="/Home_bg.svg" />
+            <BodyText txt="How many kg of Carbon Dioxide would 6 shirts be equivalent to?" fontsize="20px" />
+            <OpsButton>18.2 kg</OpsButton>
+            <CorrectButton>15.6 kg</CorrectButton>
+            <OpsButton>13 kg</OpsButton>
+            <QuizStartBtn onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        q3: Number(q3) + 1
+                    }
+                })
+            }>Next Question{q3}</QuizStartBtn>
+
+
+
+            <Img img="/Person + Bubble.svg"
+                w="200"
+                h="200" />
+
+        </div>
+
+    }
+
+    if (incorrect2 === '') {
+        return <div>
+
+            <Background img="/Home_bg.svg" />
+            <BodyText txt="Ah! You got it wrong :(" fontsize="20px" />
+            <BodyText txt="The correct answer is -" fontsize="10px" />
+            <CorrectButton>15.6 kg</CorrectButton>
+
+            <Img img="/Smoke.svg"
+
+                w="900"
+                h="1750" />
+
+            <QuizStartBtn onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        q3: Number(q3) + 1
+                    }
+                })
+            }>Next Question{q3}</QuizStartBtn>
+
+
+        </div>
+
+    }
+
+    if (incorrect2 === '') {
+        return <div>
+
+
+            <Background img="/Home_bg.svg" />
+            <BodyText txt="Ah! You got it wrong :(" fontsize="20px" />
+            <BodyText txt="The correct answer is -" fontsize="10px" />
+            <CorrectButton>15.6 kg</CorrectButton>
+
+
+            <Img img="/Smoke.svg"
+
+                w="900"
+                h="1750" />
+            <QuizStartBtn onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        q3: Number(q3) + 1
+                    }
+                })
+            }>Next Question{q3}</QuizStartBtn>
+
+        </div>
+
+    }
+
+    //Question 2
+    if (q2 === '') {
+        return <div>
+
+            <Background img="/Home_bg.svg" />
+            <BodyText txt="How many kg of Carbon Dioxide would 6 shirts be equivalent to?" fontsize="20px" />
+            <OpsButton onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        incorrect2: Number(incorrect2) + 1
+                    }
+                })
+            }>18.2 kg</OpsButton>
+
+            <OpsButton onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        correct2: Number(correct2) + 1
+                    }
+                })
+
+            }>15.6 kg{correct2}</OpsButton>
+
+            <OpsButton onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        incorrect2: Number(incorrect2) + 1
+                    }
+                })
+            }>13 kg</OpsButton>
+
+
+        </div>
+
+    }
 
     //Question 1 results
     if (correct === '') {
@@ -69,7 +318,17 @@ export default function Question1() {
             <CorrectButton>37,500 Litres</CorrectButton>
             <OpsButton>54,500 Litres</OpsButton>
             <OpsButton>64,000 Litres</OpsButton>
-            <QuizStartBtn>Next Question</QuizStartBtn>
+            <QuizStartBtn onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        q2: Number(q2) + 1
+                    }
+                })
+            }>Next Question{q2}</QuizStartBtn>
+
+
+
             <Img img="/Person + Bubble.svg"
                 w="200"
                 h="200" />
@@ -95,6 +354,15 @@ export default function Question1() {
                 w="900"
                 h="1750" />
 
+            <QuizStartBtn onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        q2: Number(q2) + 1
+                    }
+                })
+            }>Next Question{q2}</QuizStartBtn>
+
 
         </div>
 
@@ -117,13 +385,20 @@ export default function Question1() {
 
                 w="900"
                 h="1750" />
-
+            <QuizStartBtn onClick={
+                () => r.replace({
+                    pathname: "/Questions",
+                    query: {
+                        q2: Number(q2) + 1
+                    }
+                })
+            }>Next Question{q2}</QuizStartBtn>
 
         </div>
 
     }
 
-
+    
     // Question 1
     return <div>
         <Background img="/Home_bg.svg" />
@@ -155,7 +430,6 @@ export default function Question1() {
                 }
             })
         }>64,000 Litres</OpsButton>
-
 
     </div>
 
